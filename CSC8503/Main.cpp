0bc0af9baed10e08eb30f7db941366a78a00e6c5
Joseph Hydon <j.d.hydon@gmail.com>
2023-11-23 18:18:13 +0000

    networking test

--- CSC8503/Main.cpp
+++ CSC8503/Main.cpp
@@ -37,6 +37,56 @@ void TestPathfinding() {
 void DisplayPathfinding() {
 }
 
+class TestPacketReceiver : public PacketReceiver {
+public:
+    explicit TestPacketReceiver(std::string n) {
+        this->name = n;
+    }
+
+    void ReceivePacket(int type, GamePacket* payload, int source) override {
+        if (type == String_Message) {
+            StringPacket* realPacket = (StringPacket*)payload;
+
+            std::string msg = realPacket->GetStringFromData();
+
+            std::cout << name << " recieved message: " << msg << std::endl;
+        }
+    }
+protected:
+    std::string name;
+};
+
+void TestNetworking() {
+    NetworkBase::Initialise();
+
+    TestPacketReceiver serverReciever("Server");
+    TestPacketReceiver clientReciever("Client");
+
+    int port = NetworkBase::GetDefaultPort();
+
+    GameServer* server = new GameServer(port, 1);
+    GameClient* client = new GameClient();
+
+    server->RegisterPacketHandler(String_Message, &serverReciever);
+    client->RegisterPacketHandler(String_Message, &clientReciever);
+
+    auto test1 = StringPacket("Server says hello! " + std::to_string(12));
+    auto test2 = StringPacket("Server says hello! " + std::to_string(12));
+
+    bool canConnect = client->Connect(127, 0, 0, 1, port);
+    for (int i = 0; i < 100; i++) {
+        server->SendGlobalPacket(test1);
+        client->SendPacket(test2);
+
+        server->UpdateServer();
+        client->UpdateClient();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    NetworkBase::Destroy();
+}
+
 /*
 
 The main function should look pretty familar to you!
@@ -50,37 +100,38 @@ hide or show the
 
 */
 int main() {
-	Window*w = Window::CreateGameWindow("CSC8503 Game technology!", 1920, 1080);
-
-	if (!w->HasInitialised()) {
-		return -1;
-	}	
-
-	w->ShowOSPointer(false);
-	w->LockMouseToWindow(true);
-
-	TutorialGame* g = new TutorialGame();
-	w->GetTimer().GetTimeDeltaSeconds(); //Clear the timer so we don't get a larget first dt!
-	while (w->UpdateWindow() && !Window::GetKeyboard()->KeyDown(KeyCodes::ESCAPE)) {
-		float dt = w->GetTimer().GetTimeDeltaSeconds();
-		if (dt > 0.1f) {
-			std::cout << "Skipping large time delta" << std::endl;
-			continue; //must have hit a breakpoint or something to have a 1 second frame time!
-		}
-		if (Window::GetKeyboard()->KeyPressed(KeyCodes::PRIOR)) {
-			w->ShowConsole(true);
-		}
-		if (Window::GetKeyboard()->KeyPressed(KeyCodes::NEXT)) {
-			w->ShowConsole(false);
-		}
-
-		if (Window::GetKeyboard()->KeyPressed(KeyCodes::T)) {
-			w->SetWindowPosition(0, 0);
-		}
-
-		w->SetTitle("Gametech frame time:" + std::to_string(1000.0f * dt));
-
-		g->UpdateGame(dt);
-	}
-	Window::DestroyGameWindow();
+    TestNetworking();
+//	Window*w = Window::CreateGameWindow("CSC8503 Game technology!", 1920, 1080);
+//
+//	if (!w->HasInitialised()) {
+//		return -1;
+//	}
+//
+//	w->ShowOSPointer(false);
+//	w->LockMouseToWindow(true);
+//
+//	TutorialGame* g = new TutorialGame();
+//	w->GetTimer().GetTimeDeltaSeconds(); //Clear the timer so we don't get a larget first dt!
+//	while (w->UpdateWindow() && !Window::GetKeyboard()->KeyDown(KeyCodes::ESCAPE)) {
+//		float dt = w->GetTimer().GetTimeDeltaSeconds();
+//		if (dt > 0.1f) {
+//			std::cout << "Skipping large time delta" << std::endl;
+//			continue; //must have hit a breakpoint or something to have a 1 second frame time!
+//		}
+//		if (Window::GetKeyboard()->KeyPressed(KeyCodes::PRIOR)) {
+//			w->ShowConsole(true);
+//		}
+//		if (Window::GetKeyboard()->KeyPressed(KeyCodes::NEXT)) {
+//			w->ShowConsole(false);
+//		}
+//
+//		if (Window::GetKeyboard()->KeyPressed(KeyCodes::T)) {
+//			w->SetWindowPosition(0, 0);
+//		}
+//
+//		w->SetTitle("Gametech frame time:" + std::to_string(1000.0f * dt));
+//
+//		g->UpdateGame(dt);
+//	}
+//	Window::DestroyGameWindow();
 }
